bdcabf93f6cdd72dca156e6b77cbcd12a5ad3d26
Collin Meyer <colmeye@siue.edu>
2021-03-28 01:30:14 -0500

    Cards are matched and randomized! Using fisher-yates algorithm

--- HW2MeyerMemoryGame/app/src/main/java/com/example/hw2meyermemorygame/MainActivity.java
+++ HW2MeyerMemoryGame/app/src/main/java/com/example/hw2meyermemorygame/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     // Table setup
@@ -16,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
     // Card setup
     private Button[][] cardArray;
+    private Button[][] matchArray;
+
+    // Emojis
+    private int[] emojiUnicodes = new int[]{0x1F34C, 0x1F34D, 0x1F354, 0x1F355, 0x1F357, 0x1F358, 0x1F35F};
+    private String getEmojiByUnicode(int unicode){
+        return new String(Character.toChars(unicode));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +34,21 @@ public class MainActivity extends AppCompatActivity {
         // Table setup
         tl = findViewById(R.id.tableOfCards);
         tableRowCount = 3;
-        tableColCount = 3;
+        tableColCount = 4;
 
         // Card setup
         cardArray = new Button[tableRowCount][tableColCount];
         generateCards();
-
     }
 
     private void generateCards() {
+
+        int buttonCount = tableRowCount * tableColCount;
+        int matchCount = buttonCount / 2;
+
+        matchArray = new Button[ matchCount ][2];
+        int matchCounter = 0;
+        int pairCounter = 0;
 
         // For each row
         for (int i = 0; i < tableRowCount; i++) {
@@ -47,15 +62,50 @@ public class MainActivity extends AppCompatActivity {
 
                 // Create a button for the row and column
                 cardArray[i][q] = new Button(this);
-                cardArray[i][q].setText("Test");
+
+                // Pair that button with another
+                if (pairCounter > 1) {
+                    pairCounter = 0;
+                    matchCounter++;
+                }
+                matchArray[matchCounter][pairCounter] = cardArray[i][q];
+                pairCounter++;
 
                 // Add that button to the current row
                 tr.addView(cardArray[i][q]);
-
             }
 
         }
 
+        Button[][] shuffledArray = shuffle(matchArray);
+
+        for (int i = 0; i < matchCount; i++) {
+            shuffledArray[i][0].setText( getEmojiByUnicode( emojiUnicodes[i] ) );
+            shuffledArray[i][1].setText( getEmojiByUnicode( emojiUnicodes[i] ) );
+        }
+
+
     }
+
+
+    public Button[][] shuffle(Button[][] a) {
+        Random random = new Random();
+
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = a[i].length - 1; j > 0; j--) {
+                int m = random.nextInt(i + 1);
+                int n = random.nextInt(j + 1);
+
+                Button temp = a[i][j];
+                a[i][j] = a[m][n];
+                a[m][n] = temp;
+            }
+        }
+
+        return a;
+    }
+
+
+
 }
 
